06f245881b24144972f7e405d23e75bf7a1bdc73
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-20 18:31:54 +0530

    Create LikeKeyword.sql

---
+++ LikeKeyword.sql
@@ -0,0 +1,6 @@
+-- Like-- 
+select * from employee where emp_name Like '%i';
+select * from employee where emp_name Like 'i%';
+select * from employee where emp_role Like '___a%';
+select * from employee where emp_name Like '%';
+select * from employee where emp_name Like '__\%%';
