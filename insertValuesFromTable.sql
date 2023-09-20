b7b25da613915747581515276105b79b873947f5
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-20 18:29:22 +0530

    Create insertValuesFromTable.sql

---
+++ insertValuesFromTable.sql
@@ -0,0 +1,12 @@
+insert into employee values
+(1,"hari","hr",10000),
+(2,"hari","hr",30000),
+(3,"hari","hr",10000),
+(4,"hari","hr",90000),
+(5,"hari","hr",20000),
+(6,"hari","hr",40000),
+(7,"hari","hr",30000),
+(8,"hari","hr",70000),
+(9,"hari","hr",50000),
+(10,"hari","hr",20000);
+insert into employee values(11,"mani","manager",50000);
