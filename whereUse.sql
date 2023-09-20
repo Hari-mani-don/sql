6cf225e55076afa06ca34d44775e05f5b8cf5121
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-20 18:30:14 +0530

    Create whereUse.sql

---
+++ whereUse.sql
@@ -0,0 +1,5 @@
+select * from employee where emp_id =1;
+select * from employee where emp_salary =10000;
+select * from employee where emp_salary =20000;
+select * from employee where emp_name ="mani";
+select * from employee where emp_role ="manager";
