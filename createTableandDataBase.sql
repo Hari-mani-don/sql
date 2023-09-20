f5893b0aa38f5ea66b875656820cbd0c7fb8cc01
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-20 18:28:31 +0530

    Create createTableandDataBase.sql

---
+++ createTableandDataBase.sql
@@ -0,0 +1,9 @@
+create database HariMani;
+drop database harimani;
+use harimani;
+create table employee(
+emp_id int auto_increment primary key,
+emp_name varchar(20),
+emp_role varchar(10),
+emp_salary int
+);
