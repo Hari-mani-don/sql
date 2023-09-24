63a6e49aac463f4ddfa5091436096cfda4a19422
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-24 20:20:36 +0530

    Create sql20.sql

---
+++ sql20.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE jobsss(
+job_id VARCHAR(2) NOT NULL PRIMARY KEY,
+JOB_TITLE VARCHAR(20),
+MIN_SALARY DECIMAL(6,2),
+MAX_SALARY DECIMAL(6,2)
+);
+desc jobsss;
+CREATE TABLE employees (
+employee_id VARCHAR(2) primary key,
+first_name VARCHAR (20),
+last_name VARCHAR (20),
+job_id VARCHAR(2),
+salary DECIMAL(7,2),
+FOREIGN KEY(job_id) REFERENCES jobsss(job_id)ON DELETE NO ACTION  ON UPDATE NO ACTION 
+);
+Desc employees
