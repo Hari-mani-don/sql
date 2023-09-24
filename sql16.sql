b9f20f070ee5fed3bad0a33d006c248afdf5058f
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-24 19:24:14 +0530

    Create sql16.sql

---
+++ sql16.sql
@@ -0,0 +1,33 @@
+CREATE TABLE detail (
+    department_id DECIMAL(5, 0) PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+    manager_id DECIMAL(5, 0) DEFAULT NULL,
+    location_id DECIMAL(5, 0) -- Fixed the spelling of 'location_id'
+);
+ drop table detail;
+ CREATE INDEX inx_number ON detail(department_id);
+CREATE INDEX idx_department_id ON detail (department_id);
+
+CREATE TABLE employees (
+    employee_id VARCHAR(2) NOT NULL PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    hire_date DATE NOT NULL,
+    job_id VARCHAR(10) NOT NULL,
+    salary DECIMAL(7, 2) NOT NULL,
+    commission DECIMAL(5, 2) NOT NULL,
+    manager_id DECIMAL(5, 0) DEFAULT NULL, -- Use DECIMAL for manager_id
+    department_id DECIMAL(5, 0) DEFAULT NULL, -- Use DECIMAL for department_id
+    FOREIGN KEY (department_id) REFERENCES detail(department_id),
+    FOREIGN KEY (job_id)  REFERENCES jobs(job_id)
+);
+desc employees;
+ CREATE TABLE IF NOT EXISTS jobs(
+		job_id VARCHAR(2) NOT NULL UNIQUE,
+		job_title VARCHAR(30) NOT NULL default '',
+		min_salary DECIMAL(5,0) default '8000',
+        max_salary DECIMAL(5,0) 
+		);
+        desc jobs;
