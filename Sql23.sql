b32853935fe129bd856fcc09f8e95ba6c74b57b9
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-25 07:12:11 +0530

    Create Sql23.sql

---
+++ Sql23.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE cu;
+USE cu;
+CREATE TABLE  IF NOT EXISTS customer(
+customer_id INT PRIMARY KEY,
+cust_name    VARCHAR(20),
+city    VARCHAR(20),
+grade INT,
+salesman_id INT
+);
+DESC customer;
+
+INSERT INTO customer VALUES(3002 ," Nick Rimando","New York",100 ,5001);
+SELECT * FROM customer;
+SELECT * FROM customer WHERE city = 'Ney York' OR grade>100;
