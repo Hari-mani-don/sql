a466ef06e2bf6382ae10024a018a736b6d704021
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-26 11:30:48 +0530

    Create sql95.sql

---
+++ sql95.sql
@@ -0,0 +1,6 @@
+SELECT ord_no, cust_name, commission AS "Commission%",
+purch_amt*commission AS "Commission"
+FROM salesman,orders,customer
+WHERE orders.customer_id = customer.customer_id
+AND orders.salesman_id = salesman.salesman_id
+AND customer.grade>=200;
